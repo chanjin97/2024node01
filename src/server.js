6b7f5e9a29ab9141afc5b863a7c90e8fb45da44c
chanjin97 <chanjindark@naver.com>
2024-03-21 12:39:07 +0900

    BlogRouter

--- src/server.js
+++ src/server.js
@@ -4,17 +4,20 @@ const app = express();
 
 const dotenv = require("dotenv");
 const { userRouter } = require("./routes/userRouter");
+const { blogRouter } = require("./routes/blogRouter");
 
 dotenv.config();
 
 const server = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("db 연결완료");
-
+    console.log("DB 연결완료");
+    mongoose.set("debug", true);
     app.use(express.json());
 
     app.use("/user", userRouter);
+
+    app.use("/blog", blogRouter);
 
     app.listen(3000);
   } catch (error) {
